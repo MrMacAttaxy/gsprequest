0b9f99b74849c80175a128c57144eed568c1eb0d
MrMacAttaxy <116235140+MrMacAttaxy@users.noreply.github.com>
2024-12-14 15:07:26 -0500

    Update uv.config.js

--- uv.config.js
+++ uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: '/',
+    prefix: '/search',
     bare:'https://bare.benrogo.net',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
